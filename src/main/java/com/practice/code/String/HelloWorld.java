e26a71003852da12fd6212df1b61aeb789254a2d
Sheen <sheendullu13@gmail.com>
2020-09-23 02:04:24 -0700

    Hello World

--- src/main/java/com/practice/code/String/HelloWorld.java
+++ src/main/java/com/practice/code/String/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.practice.code.String;
 
-public class IsUnique {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
