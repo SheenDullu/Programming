bf3ba2a86fe72095e52a3533e7a7acfdbf715b40
Sheen <sheendullu13@gmail.com>
2020-10-01 23:07:42 -0700

    LeetCode 1171

---
+++ src/main/java/com/practice/code/tree/ZigZagTreeTraversal.java
@@ -0,0 +1,46 @@
+package com.practice.code.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Leetcode 103. Binary Tree Zigzag Level Order Traversal
+ **/
+
+public class ZigZagTreeTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> output = new ArrayList<>();
+        queue.offer(root);
+        int height = 1;
+        int nodeToVisit = 1;
+        int numOfNode = 0;
+        LinkedList<Integer> result = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            if (height % 2 == 0) result.addFirst(curr.value);
+            else result.add(curr.value);
+            nodeToVisit--;
+            if (curr.left != null) {
+                queue.offer(curr.left);
+                numOfNode++;
+            }
+            if (curr.right != null) {
+                queue.offer(curr.right);
+                numOfNode++;
+            }
+            if (nodeToVisit == 0) {
+                nodeToVisit = numOfNode;
+                numOfNode = 0;
+                height++;
+                output.add(new ArrayList<>(result));
+                result = new LinkedList<>();
+            }
+        }
+        return output;
+    }
+}
